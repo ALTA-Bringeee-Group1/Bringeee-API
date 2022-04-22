475ac9c84b2254041041cf8c53ffb8689855f5fe
Badar Wildani <wildanie12@gmail.com>
2022-04-23 02:32:46 +0700

    add: migration

--- utils/migrate.go
+++ utils/migrate.go
@@ -9,5 +9,13 @@ import (
 func Migrate(db *gorm.DB) {
 	db.AutoMigrate(
 		&entities.User{},
+		&entities.TruckType{},
+		&entities.Driver{},
+		&entities.Destination{},
+		&entities.OrderHistory{},
+		&entities.Order{},
+		&entities.District{},
+		&entities.City{},
+		&entities.Province{},
 	)
 }
