b5bde5dfec588cff0298445f04a17feee1700fe3
nasrul0807 <nasrulmuhammad748@gmail.com>
2022-04-24 00:20:29 +0700

    feat: configuring routes and server.go for create customer

--- deliveries/routes/route.go
+++ deliveries/routes/route.go
@@ -1,1 +1,12 @@
 package routes
+
+import (
+	"bringeee-capstone/deliveries/handlers"
+
+	"github.com/labstack/echo/v4"
+)
+
+func RegisterCustomerRoute(e *echo.Echo, userHandler *handlers.UserHandler) {
+	group := e.Group("/api/customers")
+	group.POST("", userHandler.CreateCustomer) // Registration
+}
