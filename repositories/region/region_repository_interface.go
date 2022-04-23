bb06c9882093512384815fcf458606b52468a749
Badar Wildani <wildanie12@gmail.com>
2022-04-23 19:50:07 +0700

    add: region repository interface

---
+++ repositories/region/region_repository_interface.go
@@ -0,0 +1,36 @@
+package region
+
+import "bringeee-capstone/entities"
+
+type RegionRepositoryInterface interface {
+	
+	/*
+	 * Find All Province
+	 * -------------------------------
+	 * Mengambil semua data provinsi
+	 *
+	 * @var sort		sort data, { field, sort[bool] }
+	 * @return Province	list provinsi dalam entity domain
+	 */
+	findAllProvince(sort []map[string]interface{}) ([]entities.Province, error)
+
+	/*
+	 * Find All City
+	 * -------------------------------
+	 * Mengambil semua data kota berdasarkan provinsi
+	 *
+	 * @var sort		sort data, { field, sort[bool] }
+	 * @return City		list kota dalam entity domain
+	 */
+	findAllCity(provinceID int, sort []map[string]interface{}) ([]entities.City, error)
+
+	/*
+	 * Find All District
+	 * -------------------------------
+	 * Mengambil semua data kecamatan berdasarkan kota
+	 *
+	 * @var sort		sort data, { field, sort[bool] }
+	 * @return District	list kecamatan dalam entity domain 
+	 */
+	findAllDistrict(cityID int, sort []map[string]interface{}) ([]entities.District, error)
+}
