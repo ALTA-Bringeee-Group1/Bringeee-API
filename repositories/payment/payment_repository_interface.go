7e4cbef057340a7f476d97eced4ba8340e76cc66
Badar Wildani <wildanie12@gmail.com>
2022-04-23 19:49:27 +0700

    add: payment repository interface

---
+++ repositories/payment/payment_repository_interface.go
@@ -0,0 +1,58 @@
+package payment
+
+import "bringeee-capstone/entities"
+
+type PaymentRepositoryInterface interface {
+
+
+	/*
+	 * Create Bank Transfer BCA
+	 * -------------------------------
+	 * Buat pembayaran untuk order tertentu menggunakan BCA
+	 * Contoh referensi: https://docs.midtrans.com/en/core-api/bank-transfer
+	 *
+	 * @var order	Entity domain order yang dibuatkan pembayaran
+	 * @return any	Response dari layanan pihak ketiga
+	 */
+	CreateBankTransferBCA(order entities.Order) (interface{}, error)
+
+	/*
+	 * Create Bank Transfer BNI
+	 * -------------------------------
+	 * Buat pembayaran untuk order tertentu menggunakan BNI
+	 *
+	 * @var order	Entity domain order yang dibuatkan pembayaran
+	 * @return any	Response dari layanan pihak ketiga
+	 */
+	CreateBankTransferBNI(order entities.Order) (interface{}, error)
+
+	/*
+	 * Create Bank Transfer BRI
+	 * -------------------------------
+	 * Buat pembayaran untuk order tertentu menggunakan BNI
+	 *
+	 * @var order	Entity domain order yang dibuatkan pembayaran
+	 * @return any	Response dari layanan pihak ketiga
+	 */
+	CreateBankTransferBRI(order entities.Order) (interface{}, error)
+
+	/*
+	 * Create Bank Transfer BRI
+	 * -------------------------------
+	 * Buat pembayaran untuk order tertentu menggunakan Mandiri
+	 *
+	 * @var order	Entity domain order yang dibuatkan pembayaran
+	 * @return any	Response dari layanan pihak ketiga
+	 */
+	CreateBankTransferMandiri(order entities.Order) (interface{}, error)
+
+	/*
+	 * Create Bank Transfer BRI
+	 * -------------------------------
+	 * Buat pembayaran untuk order tertentu menggunakan Permata
+	 *
+	 * @var order	Entity domain order yang dibuatkan pembayaran
+	 * @return any	Response dari layanan pihak ketiga
+	 */
+	CreateBankTransferPermata(order entities.Order) (interface{}, error)
+}
