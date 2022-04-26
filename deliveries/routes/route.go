b0c8b6a6d53693d433f8a48f82f8b2719db4846d
nasrul0807 <nasrulmuhammad748@gmail.com>
2022-04-26 19:09:51 +0700

    feat: configuring fo auth feature in server.go and route

--- deliveries/routes/route.go
+++ deliveries/routes/route.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"bringeee-capstone/deliveries/handlers"
+	middleware "bringeee-capstone/deliveries/middlewares"
 
 	"github.com/labstack/echo/v4"
 )
@@ -13,4 +14,8 @@ func RegisterCustomerRoute(e *echo.Echo, userHandler *handlers.UserHandler) {
 
 func RegisterTruckTypeRoute(e *echo.Echo, truckTypeHandler *handlers.TruckTypeHandler) {
 	e.GET("/api/truck_types", truckTypeHandler.Index)
+}
+func RegisterAuthRoute(e *echo.Echo, authHandler *handlers.AuthHandler) {
+	e.POST("/api/auth", authHandler.Login)
+	e.GET("/api/auth/me", authHandler.Me, middleware.JWTMiddleware())
 }
