a70f09bf613720c3de770bb90f32c7c9280ee0fa
Badar Wildani <wildanie12@gmail.com>
2022-04-28 04:11:18 +0700

    fix: user service contracts compatibility issues

--- services/user/user_services_interface.go
+++ services/user/user_services_interface.go
@@ -13,10 +13,10 @@ type UserServiceInterface interface {
 	GetPaginationCustomer(limit, page int, filters []map[string]string) (web.Pagination, error)
 	FindCustomer(id int) (entities.CustomerResponse, error)
 	DeleteCustomer(id int) error
-	FindAllDriver(limit, page int, filters []map[string]string, sorts []map[string]interface{}) ([]entities.DriverAuthResponse, error)
+	FindAllDriver(limit, page int, filters []map[string]string, sorts []map[string]interface{}) ([]entities.DriverResponse, error)
 	CreateDriver(driverRequest entities.CreateDriverRequest, files map[string]*multipart.FileHeader) (entities.DriverAuthResponse, error)
-	UpdateDriver(driverRequest entities.UpdateDriverRequest, id int, files []*multipart.FileHeader) (entities.DriverResponse, error)
-	UpdateDriverByAdmin(driverRequest entities.UpdateDriverByAdminRequest, id int, files []*multipart.FileHeader) (entities.DriverResponse, error)
+	UpdateDriver(driverRequest entities.UpdateDriverRequest, id int, files map[string]*multipart.FileHeader) (entities.DriverResponse, error)
+	UpdateDriverByAdmin(driverRequest entities.UpdateDriverByAdminRequest, id int, files map[string]*multipart.FileHeader) (entities.DriverResponse, error)
 	GetPaginationDriver(limit, page int, filters []map[string]string) (web.Pagination, error)
 	FindDriver(id int) (entities.DriverResponse, error)
 	DeleteDriver(id int) error
