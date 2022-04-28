c4b8c0b435a48e75117a1c2504cc829456faef5f
nasrul0807 <nasrulmuhammad748@gmail.com>
2022-04-28 13:38:47 +0700

    add an error message

--- deliveries/helpers/helper.go
+++ deliveries/helpers/helper.go
@@ -1,6 +1,10 @@
 package helpers
 
-import "golang.org/x/crypto/bcrypt"
+import (
+	"bringeee-capstone/entities/web"
+
+	"golang.org/x/crypto/bcrypt"
+)
 
 func ResponseFailed(message string) map[string]interface{} {
 	return map[string]interface{}{
@@ -17,4 +21,13 @@ func HashPassword(password string) (string, error) {
 func CheckPasswordHash(password, hash string) bool {
 	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
 	return err == nil
+}
+
+func MakeErrorResponse(status string, code int, err string, links map[string]string) web.ErrorResponse {
+	return web.ErrorResponse{
+		Status: status,
+		Code:   code,
+		Error:  err,
+		Links:  links,
+	}
 }
