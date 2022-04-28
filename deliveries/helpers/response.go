8c0a16b623a920c3e5b9f499730f01823805c265
Badar Wildani <wildanie12@gmail.com>
2022-04-28 11:32:07 +0700

    add: web error response helper

---
+++ deliveries/helpers/response.go
@@ -0,0 +1,27 @@
+package helpers
+
+import (
+	"bringeee-capstone/entities/web"
+	"net/http"
+	"reflect"
+
+	"github.com/labstack/echo/v4"
+)
+
+func WebErrorResponse(c echo.Context, err error, links map[string]string) error {
+	if reflect.TypeOf(err).String() == "web.WebError" {
+		webErr := err.(web.WebError)
+		return c.JSON(webErr.Code, web.ErrorResponse{
+			Status: "ERROR",
+			Code: webErr.Code,
+			Error: webErr.Error(),
+			Links: links,
+		})
+	}
+	return c.JSON(http.StatusInternalServerError, web.ErrorResponse{
+		Status: "ERROR",
+		Code: http.StatusInternalServerError,
+		Error: "Server Error",
+		Links: links,
+	})
+} 
