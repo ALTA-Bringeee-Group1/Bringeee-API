ce1aba90a579ad0faabaec91d4855c1ae5a47aa9
Badar Wildani <wildanie12@gmail.com>
2022-04-29 19:20:01 +0700

    add: additional payment env configuration

--- configs/config.go
+++ configs/config.go
@@ -25,6 +25,11 @@ type AppConfig struct {
 		AccessKey string
 		SecretKey string
 	}
+	Payment struct {
+		MidtransBaseURLProduction 	string
+		MidtransBaseURLDevelopment 	string
+		MidtransServerKey		 	string
+	}
 }
 
 var appConfig *AppConfig
@@ -46,15 +51,21 @@ func initConfig() *AppConfig {
 		config.App.Port = "8000"
 		config.App.BaseURL = "localhost:" + config.App.Port
 		config.App.ENV = ""
+
 		config.Database.Host = "localhost"
 		config.Database.Port = "3306"
 		config.Database.Username = "root"
 		config.Database.Password = "root"
 		config.Database.Name = "bringeee"
+
 		config.AwsS3.Bucket = ""
 		config.AwsS3.Region = ""
 		config.AwsS3.AccessKey = ""
 		config.AwsS3.SecretKey = ""
+
+		config.Payment.MidtransBaseURLDevelopment = ""
+		config.Payment.MidtransBaseURLProduction = ""
+		config.Payment.MidtransServerKey = ""
 
 		return &config
 	}
@@ -63,15 +74,21 @@ func initConfig() *AppConfig {
 	config.App.Port = os.Getenv("APP_PORT")
 	config.App.BaseURL = os.Getenv("APP_BASE_URL")
 	config.App.ENV = os.Getenv("APP_ENV")
+
 	config.Database.Host = os.Getenv("DB_HOST")
 	config.Database.Port = os.Getenv("DB_PORT")
 	config.Database.Username = os.Getenv("DB_USERNAME")
 	config.Database.Password = os.Getenv("DB_PASSWORD")
 	config.Database.Name = os.Getenv("DB_NAME")
+
 	config.AwsS3.Bucket = os.Getenv("AWS_S3_BUCKET")
 	config.AwsS3.Region = os.Getenv("AWS_S3_REGION")
 	config.AwsS3.AccessKey = os.Getenv("AWS_S3_ACCESS_KEY")
 	config.AwsS3.SecretKey = os.Getenv("AWS_S3_SECRET_KEY")
+
+	config.Payment.MidtransBaseURLProduction = os.Getenv("MIDTRANS_BASE_URL_PRODUCTION")
+	config.Payment.MidtransBaseURLDevelopment = os.Getenv("MIDTRANS_BASE_URL_DEVELOPMENT")
+	config.Payment.MidtransServerKey = os.Getenv("MIDTRANS_SERVER_KEY")
 
 	return &config
 }
